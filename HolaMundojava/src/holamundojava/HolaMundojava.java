b6d25edf5b9b2351159f2cf4d3369a4c65e504aa
luis <lperezhurtado97@gmail.com>
2021-11-22 19:29:08 +0100

    nuevo codigo añadido

--- HolaMundojava/src/holamundojava/HolaMundojava.java
+++ HolaMundojava/src/holamundojava/HolaMundojava.java
@@ -15,7 +15,10 @@ public class HolaMundojava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hola Mundo");
+        System.out.println("Que tal?");
+        
     }
     
 }
